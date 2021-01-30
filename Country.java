d1584644c0e8cd1b1f298b0463ac01a7d60b1216
satyanarayana-k <satya.xworkz@gmail.com>
2021-01-30 12:39:14 +0530

    adding method java files

---
+++ Country.java
@@ -0,0 +1,12 @@
+class Country{
+public static void main(String args[]){
+developer();
+developer();
+developer();
+developer();
+
+}
+public static void developer(){
+System.out.println("developing smart cities");
+}
+}
---
+++ Mask.java
@@ -0,0 +1,17 @@
+class Mask{
+public static void main(String arg[]){
+char y='s'; 
+   protect(y);
+}
+public static void protect(char X){
+if(X=='s')
+{
+System.out.println("safe to go");
+}
+else 
+{
+System.out.println("pay the fine");
+}
+}
+	
+}
